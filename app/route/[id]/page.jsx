816dcad96e6e6bf9f48202d169bda3628596479d
Olli Jokinen <olli.jokinen@iki.fi>
2025-01-18 19:33:18 +0200

    Add Sailmate logo

--- app/route/[id]/page.jsx
+++ app/route/[id]/page.jsx
@@ -14,18 +14,21 @@ const Page = () => {
         <br />
         <Link href={'https://www.sailmate.com/kayttoohjeet'}>www.sailmate.com/kayttoohjeet</Link>
       </div>
-      <h2>Lataa sovellus</h2>
-      <Link passHref href={'https://play.google.com/store/apps/details?id=fi.nautics.hybridsailmate'}>
-        <Image alt="Google Play icon" src="/images/en_badge_web_generic.png" width="200" height="200" />
-      </Link>
-      <Link passHref href={'https://apps.apple.com/fi/app/nautics-sailmate/id1536945201'}>
-        <Image
-          alt="App Store Badge"
-          src="/images/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
-          width="160"
-          height="160"
-        />
-      </Link>
+      <div className="paragraph">
+        <h2>Lataa sovellus</h2>
+        <Image alt="Sailmate logo" src="/images/logo.webp" width="64" height="64" />
+        <Link passHref href={'https://play.google.com/store/apps/details?id=fi.nautics.hybridsailmate'}>
+          <Image alt="Google Play icon" src="/images/en_badge_web_generic.png" width="200" height="200" />
+        </Link>
+        <Link passHref href={'https://apps.apple.com/fi/app/nautics-sailmate/id1536945201'}>
+          <Image
+            alt="App Store Badge"
+            src="/images/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+            width="160"
+            height="160"
+          />
+        </Link>
+      </div>
     </div>
   )
 }
