8b13193e54abaa62c7988fbce822bb5848e8147b
Olli Jokinen <olli.jokinen@iki.fi>
2025-01-18 19:28:16 +0200

    Add Avainlippu image

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,12 +1,16 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export const Footer = () => {
   return (
     <footer>
-      Nautics Sailmate on kotimainen veneilysovellus, joka käyttää karttapohjana Traficomin merikorttia. Sailmatella on
-      yli 100 000 rekisteröitynyttä käyttäjää ja yli 15 vuoden historia suomalaisten veneilijöiden luottokumppanina.
-      Nautics Sailmate on kotimainen avainlipputuote. Lue lisää osoitteessa&nbsp;
-      <Link href={'https://www.sailmate.com/tutustu'}>www.sailmate.com/tutustu</Link>
+      <Image alt="Avainlippu" src="/images/avainlippu.avif" width="87" height="81" className="avainlippu" />
+      <div className="companyDescription">
+        Nautics Sailmate on kotimainen veneilysovellus, joka käyttää karttapohjana Traficomin merikorttia. Sailmatella
+        on yli 100 000 rekisteröitynyttä käyttäjää ja yli 15 vuoden historia suomalaisten veneilijöiden
+        luottokumppanina. Nautics Sailmate on kotimainen avainlipputuote. Lue lisää osoitteessa&nbsp;
+        <Link href={'https://www.sailmate.com/tutustu'}>www.sailmate.com/tutustu</Link>
+      </div>
     </footer>
   )
 }
